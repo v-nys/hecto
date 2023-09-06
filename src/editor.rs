5c931e62c2a87e5c351174af67197dbd03c5c49a
Vincent Nys <vincentnys@gmail.com>
2023-09-06 12:56:06 +0200

    TODO: 'separate reading...'

---
+++ src/editor.rs
@@ -0,0 +1,47 @@
+use std::io::{self, stdout}; // BIJHOUDEN: truukje self
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Self {}
+    }
+    pub fn run(&self) {
+        /* BIJHOUDEN:
+        However, by default your terminal starts in canonical mode, also called cooked mode.
+        In this mode, keyboard input is only sent to your program when the user presses ENTER.
+        This is useful for many programs: it lets the user type in a line of text,
+        use BACKSPACE to fix errors until they get their input exactly the way they want it,
+        and finally press ENTER to send it to the program.
+        But it does not work well for programs with more complex user interfaces, like text editors.
+        We want to process each key press as it comes in, so we can respond to it immediately.
+        What we want is raw mode.
+         */
+        // BIJHOUDEN: moet op stdout, niet stdin, omdat terminals gecontroleerd worden door hun writer
+        // BIJHOUDEN: toekenning is nodig om de raw terminal niet te droppen
+        let _stdout = stdout().into_raw_mode().unwrap();
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Char(c) => {
+                        if c.is_control() {
+                            println!("{:?} \r", c as u8);
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c);
+                        }
+                    }
+                    Key::Ctrl('q') => break,
+                    _ => println!("{key:?}\r"),
+                },
+                Err(e) => die(e),
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,9 @@
-use std::io::{self, stdout}; // BIJHOUDEN: truukje self
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+#![warn(clippy::all)]
+mod editor;
+use editor::Editor;
 
 fn main() {
-    /* BIJHOUDEN:
-    However, by default your terminal starts in canonical mode, also called cooked mode.
-    In this mode, keyboard input is only sent to your program when the user presses ENTER.
-    This is useful for many programs: it lets the user type in a line of text,
-    use BACKSPACE to fix errors until they get their input exactly the way they want it,
-    and finally press ENTER to send it to the program.
-    But it does not work well for programs with more complex user interfaces, like text editors.
-    We want to process each key press as it comes in, so we can respond to it immediately.
-    What we want is raw mode.
-     */
-    // BIJHOUDEN: moet op stdout, niet stdin, omdat terminals gecontroleerd worden door hun writer
-    // BIJHOUDEN: toekenning is nodig om de raw terminal niet te droppen
-    let _stdout = stdout().into_raw_mode().unwrap();
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?} \r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(e) => die(e),
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
