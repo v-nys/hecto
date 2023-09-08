56d9a24b99e3902d1d3ed3f88267980324af855d
Vincent Nys <vincent.nys@ap.be>
2023-09-08 10:05:30 +0200

    At 'move the cursor'.

--- src/editor.rs
+++ src/editor.rs
@@ -3,6 +3,8 @@ use termion::event::Key;
 
 use crate::terminal::Terminal;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
@@ -12,8 +14,6 @@ fn die(e: std::io::Error) {
     Terminal::clear_screen();
     panic!("{}", e);
 }
-
-
 
 impl Editor {
     pub fn default() -> Self {
@@ -38,26 +38,44 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        Terminal::clear_screen();
+        Terminal::cursor_hide();
+
         Terminal::cursor_position(0, 0);
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
             Terminal::cursor_position(0, 0);
         }
+        Terminal::cursor_show();
         io::stdout().flush()
     }
 
+    fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
-            println!("~\r");
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
         }
     }
 
     pub fn run(&mut self) {
-        
-
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
--- src/terminal.rs
+++ src/terminal.rs
@@ -51,4 +51,16 @@ impl Terminal {
             }
         }
     }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
 }
